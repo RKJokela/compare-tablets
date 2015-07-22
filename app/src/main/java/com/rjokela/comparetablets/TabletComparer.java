96e514ebd1866da7659ff024cb53b6dfd9af00ce
RKJokela <randyjokela@gmail.com>
2015-07-21 22:17:25 -0700

    Create TabletComparer

---
+++ app/src/main/java/com/rjokela/comparetablets/TabletComparer.java
@@ -0,0 +1,106 @@
+package com.rjokela.comparetablets;
+
+import android.content.res.Resources;
+import android.util.Log;
+
+/**
+ * Created by Randon K. Jokela on 7/21/2015.
+ */
+public class TabletComparer {
+    public static final String TAG = "TabletComparer";
+
+    // can't select more than this; will throw an exception if you try
+    public static final int MAX_SELECTED = 3;
+
+    private static TabletComparer instance = null;
+
+    Tablet[] tablets;
+    boolean[] isSelected;
+
+    private TabletComparer(Resources res, int resourceId) {
+        // get tablet ids from array resource
+        int[] tabletIds = res.getIntArray(resourceId);
+
+        // initialize tablets
+        tablets = new Tablet[tabletIds.length];
+        int i = 0;
+        for (Tablet t : tablets) t = new Tablet(res, tabletIds[i++]);
+
+        isSelected = new boolean[i];
+        unSelectAll();
+    }
+
+    public static void loadData(Resources res, int resourceId) {
+        instance = new TabletComparer(res, resourceId);
+    }
+
+    public static TabletComparer getInstance() {
+        if (instance == null)
+            Log.e(TAG, "Tablets must be loaded from xml first! Call loadData first.");
+        return instance;
+    }
+
+    /**
+     * Get a tablet from the list
+     * @param index which tablet to get
+     * @return the Tablet at specified index, or null if out of bounds
+     */
+    public Tablet getTablet(int index) {
+        if (index >= 0 && index < tablets.length) return tablets[index];
+        return null;
+    }
+
+    public int getTabletCount() { return tablets.length; }
+
+    public Tablet getSelectedTablet(int index) {
+        for (int i = 0; i < tablets.length; i++) {
+            if (isSelected[i]) {
+                if (--index < 0) {
+                    return tablets[i];
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * get the number of tablets currently selected
+     * @return how many are selected
+     */
+    public int getSelectedCount() {
+        int count = 0;
+        for (boolean b : isSelected) {
+            if (b) count++;
+        }
+        return count;
+    }
+
+    public class TooManySelectedException extends RuntimeException {
+        public TooManySelectedException() {
+            super("You may not select more than " + MAX_SELECTED + " tablets!");
+        }
+    }
+
+    public void setSelected(Tablet t, boolean selected) throws TooManySelectedException {
+        for (int i = 0; i < tablets.length; i++) {
+            if (t.equals(tablets[i])) {
+                if (selected && getSelectedCount() >= MAX_SELECTED)
+                    throw new TooManySelectedException();
+                else
+                    isSelected[i] = selected;
+            }
+        }
+    }
+
+    public void select(Tablet t) {
+        setSelected(t, true);
+    }
+
+    public void unSelect(Tablet t) {
+        setSelected(t, false);
+    }
+
+    public void unSelectAll() {
+        for (boolean b : isSelected) b = false;
+    }
+}
