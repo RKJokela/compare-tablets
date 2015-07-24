31c6d4c2d541f4efdc1ce7745a5fe054a8a43eb9
RKJokela <randyjokela@gmail.com>
2015-07-24 07:34:58 -0700

    Make dynamic layout for tablet comparison activity

--- app/src/main/java/com/rjokela/comparetablets/TabletCompareAdapter.java
+++ app/src/main/java/com/rjokela/comparetablets/TabletCompareAdapter.java
@@ -1,6 +1,7 @@
 package com.rjokela.comparetablets;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -17,11 +19,24 @@ public class TabletCompareAdapter extends ArrayAdapter<Tablet> {
 
     public static final String TAG = "TabletCompareAdapter";
 
+    private static final int IDX_IMAGE      = 0;
+    private static final int IDX_TITLE      = 1;
+    private static final int IDX_OS         = 2;
+    private static final int IDX_PRICE      = 3;
+    private static final int IDX_DIMEN      = 4;
+    private static final int IDX_WEIGHT     = 5;
+    private static final int IDX_SCREENSIZE = 6;
+    private static final int IDX_RESOLUTION = 7;
+    private static final int IDX_CHECKBOX   = 8;
+
     private final TabletComparer tabletComparer;
+
+    private Resources res;
 
     public TabletCompareAdapter(Context context, int resource, int textViewResourceId, Tablet[] objects) {
         super(context, resource, textViewResourceId, objects);
         tabletComparer = TabletComparer.getInstance();
+        res = context.getResources();
     }
 
     @Override
@@ -30,12 +45,31 @@ public class TabletCompareAdapter extends ArrayAdapter<Tablet> {
 
         //Log.d(TAG, "layout.getChildCount() returned " + layout.getChildCount());
 
-        ImageView image = (ImageView) layout.getChildAt(1);
-        image.setImageDrawable(getItem(position).getDrawable());
+        Tablet tablet = getItem(position);
 
-        CheckBox checkBox = (CheckBox) layout.getChildAt(0);
+        // set the image
+        ImageView image = (ImageView) layout.getChildAt(IDX_IMAGE);
+        image.setImageDrawable(tablet.getDrawable());
+
+        // set all the text
+        TextView tv;
+        tv = (TextView) layout.getChildAt(IDX_OS); // skip the title as this is done by the superclass
+        tv.setText(tablet.getOsVersion());
+        tv = (TextView) layout.getChildAt(IDX_PRICE);
+        tv.setText(tablet.getPrice());
+        tv = (TextView) layout.getChildAt(IDX_DIMEN);
+        tv.setText(tablet.getDimensions());
+        tv = (TextView) layout.getChildAt(IDX_WEIGHT);
+        tv.setText(tablet.getWeight());
+        tv = (TextView) layout.getChildAt(IDX_SCREENSIZE);
+        tv.setText(tablet.getScreenSize());
+        tv = (TextView) layout.getChildAt(IDX_RESOLUTION);
+        tv.setText(tablet.getResolution());
+
+        CheckBox checkBox = (CheckBox) layout.getChildAt(IDX_CHECKBOX);
         // make sure all checkboxes have different IDs???
         checkBox.setId(R.id.checkBox_id + position);
+        checkBox.setChecked(true);
         Log.d(TAG, "Getting view at position " + position + "; checkbox id is " + checkBox.getId());
         final int finalPosition = position;
         checkBox.setOnClickListener(new View.OnClickListener() {
